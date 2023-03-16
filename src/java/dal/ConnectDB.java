2100ab51f26555834ffe51b44b9206bf01f728e6
Ngo Xuan Bach <bach2002ngottbg@gmail.com>
2023-03-16 22:34:23 +0700

    Khung Thu Muc Du An

---
+++ src/java/dal/ConnectDB.java
@@ -0,0 +1,23 @@
+
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class ConnectDB {
+    protected Connection connection;
+    public ConnectDB()
+    {
+        try {
+            String url = "jdbc:sqlserver://localhost:1433;databaseName= ttcs";
+            String username = "sa";
+            String password = "123";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+}
